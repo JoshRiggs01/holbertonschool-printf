878429a4d37abb6ca40cbb85a99b77ddb68a3568
kkeas <keaskatrina@gmail.com>
2022-11-01 14:04:31 -0700

    edited main file and created printf file

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
 #ifndef main_h
 #define main_h
 
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+/*functions*/
 int _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif
