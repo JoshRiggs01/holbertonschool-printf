4afd2f1fdf6e1436b2eb03da2c92950448ba2a82
kkeas <keaskatrina@gmail.com>
2022-11-01 15:30:38 -0700

    handled conversion specifiers c and s

--- printf_functions.c
+++ printf_functions.c
@@ -11,3 +11,30 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * print_c - function
+ * @c: char
+ * Return: 1
+ */
+int print_c(va_list c)
+{
+	int a = va_arg(c, int);
+
+	_putchar(a);
+	return (1);
+}
+
+/**
+ * print_s - function
+ * @s: string
+ * Return: number of characters
+ */
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *)
+
+		for (count = 0; str[count]; count++)
+			_putchar(str[count]);
+	return (count);
+}
