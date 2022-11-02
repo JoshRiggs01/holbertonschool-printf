f8b0241b297f5b559c3071d1c0c75d130c9243a0
JoshRiggs01 <5574@holbertonstudents.com>
2022-11-01 18:18:51 -0700

    touch up t o printf.c

--- _printf.c
+++ _printf.c
@@ -1,24 +1,64 @@
 #include "main.h"
 
 /**
- * map_func - maps format specifiers to functions
- * @ap: va_list that contains args
- * @c: char (format i + 1)
- * Return: 2 (number of characters printed)
+ * check - - maps format to pointer
+ * @format: format specifier
+ *
+ * Return: pointer
  */
-int map_func(va_list ap, char c)
+int (*check(const char *format))(va_list)
 {
-	int j;
+	unsigned int j;
 	format_t f[] = {
 		{"c", print_char},
 		{"s", print_str},
 		{"d", print_int},
-		{"i", print_int}
+		{"i", print_int},
+		{NULL, NULL}
 	};
-	for (j = 0; j < 4; j++)
-		if (*f[j].let == c)
-			return (f[j].func(ap));
-	_putchar('%');
-	_putchar(c);
-	return (2);
+	for (j = 0; f[j].type != NULL; j++)
+	{
+		if (*(f[j].type) == *form)
+			break;
+	}
+	return (f[j].f);
+}
+/**
+ * _printf - prints anything
+ * @format: format specifier
+ *
+ * Return: count of char
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int x, count = 0;
+	va_list a;
+	int (*c)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	x = 0;
+	while (format[x])
+	{
+		for (i = 1; format[x] != '%' && format[x]; x++)
+		{
+			_putchar(format[x]);
+			count++;
+		}
+		if (!format[x])
+			return (count);
+		c = check(&format[x + 1]);
+		if (c != NULL)
+		{
+			count += c(a);
+			x += 2;
+			continue;
+		}
+		if (format[x + 1] == '%')
+			x += 2;
+		else
+			x++;
+	}
+	va_end(a);
+	return (count);
 }
